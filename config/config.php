5a5f94822cd9071b7b8c4104cc7b0740c6a58aa2
Sooooyunnn <yorn.soyun.1222@rupp.edu.kh>
2025-06-04 05:17:12 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -13,6 +13,10 @@
         //pass
         define("PASS", "");
 
+        $host = 'localhost'; // on cPanel, this is usually still 'localhost'
+        $dbname = 'anxiety_db'; // like 'anxiety_db'
+        $user = 'anxiety_user';   // like 'anxiety_user'
+        $pass = 'H#K7bkh2*gq='; // the password you created in cPanel
 
         $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME."", USER, PASS);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
