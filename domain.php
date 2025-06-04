c80261dda26dc0b47228bbe86d1642b4ba2bf600
Sooooyunnn <yorn.soyun.1222@rupp.edu.kh>
2025-06-04 11:50:55 +0700

    Update domain.php

--- domain.php
+++ domain.php
@@ -1,10 +1,10 @@
 <?php
 $Domain = $_SERVER['HTTP_HOST'];
 if (strpos($Domain, "localhost") !== false) { //stringPos is a function that returns the position of a string
-    define("APP_URL", "http://localhost/anxiety-coffee/"); //localhost/hotelbooking
+    define("APPURL", "http://localhost/anxiety-coffee/"); //localhost/hotelbooking
 }
 else {
-    define("APP_URL", "https://maisreyneang.com/sreyneang/anxiety-coffee/"); //www.example.com
+    define("APPURL", "https://maisreyneang.com/sreyneang/anxiety-coffee/"); //www.example.com
 }
 // echo '<script>console.log("' . APP_URL . '")</script>';
 ?>
