a79d319d30ef3d8b408e7892c7c5acc1e95a0983
Ra Vattra <vattraramo13@gmail.com>
2025-06-04 12:25:23 +0700

    update link domain.php

--- includes/domain.php
+++ includes/domain.php
@@ -1,9 +1,13 @@
 <?php
 $Domain = $_SERVER['HTTP_HOST'];
 if (strpos($Domain, "localhost") !== false) {
-    define("APPURL", "http://localhost/anxiety-coffee/");
+    define("APPURL", "/anxiety-coffee/");
+    define("IMAGEPRODUCTS", "/anxiety-coffee/admin-panel/products-admins/images");
 } else {
     define("APPURL", "https://maisreyneang.com/sreyneang/anxiety-coffee/");
+    define("IMAGEPRODUCTS", "https://maisreyneang.com/sreyneang/anxiety-coffee/admin-panel/products-admins/images");
+
+
 }
 // echo '<script>console.log("' . APPURL . '")</script>';
 ?>
