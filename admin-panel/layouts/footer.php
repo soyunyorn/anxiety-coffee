7c49795b15fb219b4d112125b0ae72cafdce4158
soyunyorn <yorn.soyun.1222@rupp.edu.kh>
2025-06-09 01:59:08 +0700

    layout

--- admin-panel/layouts/footer.php
+++ admin-panel/layouts/footer.php
@@ -1,11 +1,10 @@
-</div>
-  </div>
-</div>
-</div>
-</div>
-</div>
-<script type="text/javascript">
+    </div> <!-- Close container-fluid -->
+</div> <!-- Close #wrapper -->
 
+<!-- Optional JavaScript -->
+<script type="text/javascript">
+  // You can add custom JS here if needed
 </script>
+
 </body>
 </html>
