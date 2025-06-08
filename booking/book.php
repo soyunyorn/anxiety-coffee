2b02d4b4cf5ecec90929a26efecb0c2b1861be76
soyunyorn <yorn.soyun.1222@rupp.edu.kh>
2025-06-09 01:15:27 +0700

    sad

--- booking/book.php
+++ booking/book.php
@@ -1,9 +1,10 @@
 <?php
-require "../includes/header.php"; // Make sure session_start() is here
+ob_start();
+
+require "../includes/header.php";
 require "../config/config.php";
 
 if (isset($_POST['submit'])) {
-
     if (
         empty($_POST['first_name']) || empty($_POST['last_name']) ||
         empty($_POST['date']) || empty($_POST['time']) ||
@@ -11,6 +12,10 @@ if (isset($_POST['submit'])) {
     ) {
         echo "<script>alert('One or more inputs are empty');</script>";
     } else {
+        if (!isset($_SESSION['user_id'])) {
+            echo "<script>alert('You must be logged in to book.');</script>";
+            exit();
+        }
 
         $first_name = $_POST['first_name'];
         $last_name = $_POST['last_name'];
@@ -20,15 +25,16 @@ if (isset($_POST['submit'])) {
         $message = $_POST['message'];
         $user_id = $_SESSION['user_id'];
 
-        // Convert date input to DateTime object
         $inputDate = DateTime::createFromFormat('n/j/Y', $date);
         $today = new DateTime();
-        $today->setTime(0, 0, 0);  // Compare only date part
+        $today->setTime(0, 0, 0);
+
+        // Debug to check inputDate
+        // var_dump($inputDate, $date);
 
         if ($inputDate && $inputDate > $today) {
             $insert = $conn->prepare("INSERT INTO bookings (first_name, last_name, date, time, phone, message, user_id) 
                 VALUES (:first_name, :last_name, :date, :time, :phone, :message, :user_id)");
-
             $insert->execute([
                 ":first_name" => $first_name,
                 ":last_name" => $last_name,
@@ -39,7 +45,6 @@ if (isset($_POST['submit'])) {
                 ":user_id" => $user_id
             ]);
 
-            // Redirect to homepage after successful booking
             header("Location: https://maisreyneang.com/sreyneang/anxiety-coffee/");
             exit();
         } else {
@@ -47,4 +52,6 @@ if (isset($_POST['submit'])) {
         }
     }
 }
+
+ob_end_flush();
 ?>
