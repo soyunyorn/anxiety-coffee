3fac67fea036d9bd4d9541e7651b0d2db419f0a9
soyunyorn <yorn.soyun.1222@rupp.edu.kh>
2025-06-09 10:50:18 +0700

    dsaf

--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  
+   <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
+   <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+   <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+   <link rel="manifest" href="/images/site.webmanifest">
+   <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
     
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700" rel="stylesheet">
